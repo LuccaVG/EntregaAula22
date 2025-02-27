1bc910d14104d20e7b0569b98f1bfe8dfe5154cb
LuccaVG <112782142+LuccaVG@users.noreply.github.com>
2025-02-27 18:50:35 -0300

    Update main.py

--- CodigoFibo/main.py
+++ CodigoFibo/main.py
@@ -1,1 +1,28 @@
+from minhasoperacoes.operacoes2 import soma, subtracao, multiplicacao, divisao
+from minhasoperacoes.utils2 import exibir_resultado
 
+def main():
+    operacoes = {
+        '+': soma,
+        '-': subtracao,
+        '*': multiplicacao,
+        '/': divisao
+    }
+
+    operacao = input("Escolha uma operação (+, -, *, /): ")
+    if operacao not in operacoes:
+        print("Operação inválida")
+        return
+
+    try:
+        a = float(input("Insira o primeiro número: "))
+        b = float(input("Insira o segundo número: "))
+    except ValueError:
+        print("Entrada inválida. Por favor, insira números válidos.")
+        return
+
+    resultado = operacoes[operacao](a, b)
+    exibir_resultado(operacao, resultado)
+
+if __name__ == "__main__":
+    main()
